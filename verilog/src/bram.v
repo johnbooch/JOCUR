3ce5b1a5e0a4171af542114f4624e55a1c61c112
johnbooch <johnbuccieri@gmail.com>
2018-11-15 23:05:05 -0500

    Basic impl of brom and bram

--- verilog/src/bram.v
+++ verilog/src/bram.v
@@ -1,17 +1,26 @@
-module BRAM (
-    input ck;
-    input [DATA_WIDTH:0] addr;
-    input [DATA_WIDTH:0] din;
-    input wen;
-    output [DATA_WIDTH:0] dout;
-);
+module BRAM 
+    #(
+        DATA_WIDTH = 32,
+        BRAM_DEPTH = 256;
+    );
+    (
+        input ck;
+        input [DATA_WIDTH-1:0] addr;
+        input [DATA_WIDTH-1:0] din;
+        input wen;
+        input ce;
+        output [DATA_WIDTH-1:0] dout;
+        output busy;
+    );
 
-    reg [DATA_WIDTH:0] RAM[0:BRAM_WIDTH];
+    reg [DATA_WIDTH-1:0] RAM[0:BRAM_DEPTH-1];
 
     assign dout = RAM[addr];
 
-    always(@negedge ck) begin
-        if(wen) begin
+    assign busy =  ce ? 1'b1 ? 1'bZ
+    
+    always(@posedge ck) begin
+        if(wen && ce) begin
             RAM[addr] <= din;
         end
     end
--- verilog/src/brom.v
+++ verilog/src/brom.v
@@ -1,9 +1,29 @@
-module BROM(
-    input ck;
-    input [DATA_WIDTH:0] addr;
-    input [DATA_WIDTH:0] din;
-    input wen;
-    output [DATA_WIDTH:0] dout;
-);
+module BROM 
+    #( 
+        DATA_WIDTH = 32,
+        BROM_DEPTH = 256
+    );
+    (
+        input ck;
+        input [DATA_WIDTH-1:0] addr;
+        input [DATA_WIDTH-1:0] din;
+        input wen;
+        input ce;
+        output [DATA_WIDTH-1:0] dout;
+        output busy;
+    );
+
+    reg [DATA_WIDTH-1:0] ROM[0:BROM_DEPTH-1];
+
+    assign dout = ROM[addr];
+
+    assign busy =  ce ? 1'b1 ? 1'bZ
+
+
+    always(@posedge ck) begin
+        if(wen && ce) begin
+            ROM[addr] <= din;
+        end
+    end
 
 endmodule // BROM
